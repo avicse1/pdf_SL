23413f13f34877d5cbb9997f9aa247fcc6bef94f
Avi <avicse509@gmail.com>
2018-06-30 13:09:55 +0530

    pdf design

--- resources/views/pdf_view.blade.php
+++ resources/views/pdf_view.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
@@ -20,13 +21,13 @@
 
     <div style="display: flex;width: 750px;height:875px;overflow: hidden;background: #fff;margin: auto;flex-wrap: wrap">
         <div style="width: 500px">
-            <h3 style="font-size: 36px;font-weight: bold;color: #949599;border-bottom: 15px solid #646466;padding-bottom: 15px;height: auto;margin-top: 17px;">SERVERS</h3>
+            <h3 style="font-size: 36px;font-weight: bold;color: #949599;border-bottom: 15px solid #646466;padding-bottom: 15px;height: auto;margin-top: 17px; margin-bottom:10px;">SERVERS</h3>
             <div class="top-text" style="display: flex;margin-top: 10px;">
                 <div style="width: 40%;padding-right: 5px;padding-top:0px;font-size: 16px;color: #646466;border-right: 1px solid #D7181E;">
                     SERVERS ARE THE FOUNDATIONAL BUILDING BLOCKS OF YOUR IT INFRASTRUCTURE.
                 </div>
 
-                <div style="width: 60%;font-size: 10.5px;margin-left: 10px;color: #77787A; ">
+                <div style="width: 60%;font-size: 10.5px;margin-left: 210px;color: #77787A; ">
                     Although functions vary, they provide a centralize such as running a database. Whether they are located in your office or
                     in another location such as a data center, the servers need to be properly maintained,d location for
                     you to store and share your data and enable you to manage security. They can also be utilized to run
@@ -38,9 +39,10 @@
 
 
         </div>
-        <div style="width: 200px;margin-left: 30px;">
-            <img style="height: 70px;width:auto;margin-left: 40px;" src="logo2.png">
-            <img src="server.png" alt="">
+        <div style="width: 200px;margin-left: 30px; float:right">
+            <img style="height: 70px;width:auto; margin-top:5px;margin-left:50px;" src="./images/logo2.png">
+            <br>
+            <img style="" src="./images/server.png" alt="">
             <h4 style="margin-top:15px;color: #D7181E;font-size: 13px;
             border-bottom: 2px solid #949599;
             padding-bottom: 5px;">WHY IS THIS IMPORTANT?</h4>
