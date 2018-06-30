d6e3cfb1a5a745cf95384339435ac92c6b7385d9
Avi <avicse509@gmail.com>
2018-06-30 12:04:31 +0530

    html to pdf

---
+++ resources/views/pdf_view.blade.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+</style>
+
+<body style="background: #fff;padding-top: 40px;">
+
+    <div style="display: flex;width: 750px;height:875px;overflow: hidden;background: #fff;margin: auto;flex-wrap: wrap">
+        <div style="width: 500px">
+            <h3 style="font-size: 36px;font-weight: bold;color: #949599;border-bottom: 15px solid #646466;padding-bottom: 15px;height: auto;margin-top: 17px;">SERVERS</h3>
+            <div class="top-text" style="display: flex;margin-top: 10px;">
+                <div style="width: 40%;padding-right: 5px;padding-top:0px;font-size: 16px;color: #646466;border-right: 1px solid #D7181E;">
+                    SERVERS ARE THE FOUNDATIONAL BUILDING BLOCKS OF YOUR IT INFRASTRUCTURE.
+                </div>
+
+                <div style="width: 60%;font-size: 10.5px;margin-left: 10px;color: #77787A; ">
+                    Although functions vary, they provide a centralize such as running a database. Whether they are located in your office or
+                    in another location such as a data center, the servers need to be properly maintained,d location for
+                    you to store and share your data and enable you to manage security. They can also be utilized to run
+                    applications, host your email or to provide specialized functions patched and secured against unauthorized
+                    access.
+                </div>
+            </div>
+
+
+
+        </div>
+        <div style="width: 200px;margin-left: 30px;">
+            <img style="height: 70px;width:auto;margin-left: 40px;" src="logo2.png">
+            <img src="server.png" alt="">
+            <h4 style="margin-top:15px;color: #D7181E;font-size: 13px;
+            border-bottom: 2px solid #949599;
+            padding-bottom: 5px;">WHY IS THIS IMPORTANT?</h4>
+
+
+
+        </div>
+
+
+
+        <div class="clearfix">
+
+        </div>
+    </div>
+
+
+</body>
+
+<style>
+    body {
+        font-family: 'Myriad Pro'
+    }
+
+    img {
+        width: 100%;
+    }
+
+    .row {
+        display: flex;
+    }
+
+    .col-8 {
+        width: 80%;
+
+    }
+
+    .col-8 p {
+        padding: 15px;
+    }
+
+    .col-4 {
+        width: 20%;
+    }
+
+    .col-4 p {
+        padding-top: 150px;
+        padding-left: 15px;
+    }
+
+    .list {
+        display: flex;
+    }
+
+    .list li {
+        list-style: none;
+        width: 30%;
+        font-size: 11px;
+        color: #E6E6E6;
+        text-align: center;
+    }
+
+    .bar::after {
+        height: 100%;
+        content: '';
+        background: #000;
+        width: 30%;
+        display: block;
+        left: 70%;
+        right: 0;
+        position: absolute;
+        z-index: 5;
+    }
+
+    .bar {
+        overflow: hidden;
+        position: relative;
+    }
+</style>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('download_pdf', 'PdfController@download_pdf')->name('pdf');
